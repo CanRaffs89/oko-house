cda5e8b3949e8ce0441aa7d43161555c4ba68bb1
Can's Macbook <Can@Cans-MacBook-Air.local>
2021-10-26 21:22:37 +0100

    Section styling

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,10 +1,12 @@
 const Section = () => {
     return (
         <div className="section-container">
-            <h1>House Designs</h1>
-            <span></span>
-            <p>Eco-friendly, future-proof, self-build kit houses designed for modern living.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod minima doloribus natus molestias doloremque sint corporis. Doloribus quam deleniti nobis ullam. Quae adipisci nam voluptatibus nemo consequuntur esse corrupti eaque.</p>
+            <div className="section-text">
+                <h1>House Designs</h1>
+                <span className="span-line-text"></span>
+                <p>Eco-friendly, future-proof, self-build kit houses designed for modern living.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod minima doloribus natus molestias doloremque sint corporis. Doloribus quam deleniti nobis ullam. Quae adipisci nam voluptatibus nemo consequuntur esse corrupti eaque.</p>
+            </div>
             <div className="section-grid-1">
                 <a className="grid-sm" href="/"></a>
                 <a className="grid-md" href="/"></a>
@@ -12,6 +14,7 @@ const Section = () => {
                 <a className="grid-md" href="/"></a>
                 <a className="grid-sm" href="/"></a>
             </div>
+            <span className="span-line-bottom-border"></span>
         </div>
     )
 }
