6a53a2c4737ff640b904f4dd5e428b413ba73051
CanRaffs89 <Can@Cans-MacBook-Air.local>
2021-10-27 21:07:02 +0100

    Styled bottom border span

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,19 +3,21 @@ import Button from "./Button"
 const Section = () => {
     return (
         <div className="section-container">
-            <div className="section-text">
-                <h1>House Designs</h1>
-                <span className="span-line-text"></span>
-                <p>Eco-friendly, future-proof, self-build kit houses designed for modern living.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod minima doloribus natus molestias doloremque sint corporis. Doloribus quam deleniti nobis ullam. Quae adipisci nam voluptatibus nemo consequuntur esse corrupti eaque.</p>
-                <Button link={'/'} text={'DESIGNS'}/>
-            </div>
-            <div className="section-grid-1">
-                <a className="grid-sm" href="/"></a>
-                <a className="grid-md" href="/"></a>
-                <a className="grid-lg" href="/"></a>
-                <a className="grid-md" href="/"></a>
-                <a className="grid-sm" href="/"></a>
+            <div className="section-content-container">
+                <div className="section-text">
+                    <h1>House Designs</h1>
+                    <span className="span-line-text"></span>
+                    <p>Eco-friendly, future-proof, self-build kit houses designed for modern living.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod minima doloribus natus molestias doloremque sint corporis. Doloribus quam deleniti nobis ullam. Quae adipisci nam voluptatibus nemo consequuntur esse corrupti eaque.</p>
+                    <Button link={'/'} text={'DESIGNS'}/>
+                </div>
+                <div className="section-grid-1">
+                    <a className="grid-sm" href="/"></a>
+                    <a className="grid-md" href="/"></a>
+                    <a className="grid-lg" href="/"></a>
+                    <a className="grid-md" href="/"></a>
+                    <a className="grid-sm" href="/"></a>
+                </div>
             </div>
             <span className="span-line-bottom-border"></span>
         </div>
