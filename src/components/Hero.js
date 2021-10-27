c746658b9b648a26f2488481bb4086e06a674db1
CanRaffs89 <candice.raffel89@gmail.com>
2021-10-27 20:10:12 +0100

    Revert "Moved Button to separate component"
    This reverts commit 36b5e94c9e86b026ccf4cf534131914bdc80af46.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import Button from "./Button"
+import arrow from '../img/arrow.svg'
 
 const Hero = () => {
     return (
@@ -7,7 +7,10 @@ const Hero = () => {
                 <h1 className="hero-text">Designs for Life</h1>
                 <span></span>
                 <h3 className="hero-text-small">Eco-friendly, future-proof, self-build kit houses designed for modern living</h3>
-                <Button link={'/'} text={'MORE'}/>
+                <button>
+                    <a href="/">MORE </a>
+                    <img className="btn-arrow" src={arrow} alt="" />
+                </button>
             </div>
         </div>
     )
