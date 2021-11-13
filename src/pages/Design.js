4b1bacb85d5c6be1daac3b3889c04223dd80e834
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2021-11-13 21:12:37 +0000

    Design page grid

--- src/pages/Design.js
+++ src/pages/Design.js
@@ -5,17 +5,17 @@ const Design = ({ match, location }) => {
         <div className='container page-container'>
            <h1>Design 00{ designId }</h1>  
            <span className='designs-span'></span>
-           <p>
-                <strong>Match Props: </strong>
-                <code>{JSON.stringify(match, null, 2)}</code>      
-            </p>
-            <p>
-                <strong>Location Props: </strong>
-                <code>{JSON.stringify(location, null, 2)}</code>      
-            </p>
            <div className="design-content">
-               <div className="design-content-text"></div>
-               <div className="design-content-grid"></div>
+               <div className="design-content-text">
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, necessitatibus. Explicabo excepturi optio enim voluptates saepe nesciunt nobis illum, ab tempore similique veritatis eaque laboriosam omnis debitis, recusandae, ratione blanditiis!</p>
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus et laborum impedit magni, itaque fugiat, soluta distinctio voluptatibus eaque officiis iste repellat doloremque corrupti dolor inventore dolorem delectus, assumenda aliquid?</p>
+                   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure optio dolorem eos nesciunt praesentium adipisci nam in, assumenda recusandae perspiciatis voluptatem dignissimos quidem vitae harum amet? Obcaecati voluptatem consectetur eius!</p>
+               </div>
+               <div className="design-content-grid">
+                   <div className="design-content-grid-item-main"></div>
+                   <div className="design-content-grid-item-sm"></div>
+                   <div className="design-content-grid-item-sm"></div>
+               </div>
            </div>
         </div>
     )
